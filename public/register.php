44af27de2078d7d75b8cd94a79e2b190a589a608
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-11-05 21:01:07 +0100

    Сделана уже небольшая логика регистрации, проверка пароля, если не совпадает, то выпадает сообщение

--- public/register.php
+++ public/register.php
@@ -1,4 +1,5 @@
 <?php 
+    session_start();
 
 // страница регистрации
 
@@ -13,22 +14,25 @@
     <title>TFund</title>
 </head>
 <body>
-    <form action="" method="">
+    <form action="/fund_app/app/auth.php" method="post" enctype="multipart/form-data">
         <label>ФИО</label>
-        <input type="text" placeholder="Введите своё полное имя">
+        <input type="text" name="full_name" placeholder="Введите своё полное имя">
         <label>Логин</label>
-        <input type="text" placeholder="Введите свой логин">
+        <input type="text" name="login" placeholder="Введите свой логин">
         <label>Почта</label>
+        <input type="email" name="email" placeholder="Введите свою почту">
         <label>Изображение профиля</label>
-        <input type="file">
-        <input type="email" placeholder="Введите свою почту">
+        <input type="file" name="avatar">
         <label>Пароль</label>
-        <input type="password" placeholder="Введите пароль">
+        <input type="password" name="password" placeholder="Введите пароль">
         <label>Подтверждение пароля</label>
-        <input type="password" placeholder="Подтвердите пароль">
+        <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
         <button>Зарегестрироваться</button>
         <p>
             У вас уже есть аккаунт? - <a href="/fund_app/public/index.php">войдите</a>
+        </p>
+        <p>
+            какое то сообщение
         </p>
     </form>
 </body>
