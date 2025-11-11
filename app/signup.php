6f82859ff0b3be1d53e225a194160d834c3a281f
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-11-11 22:15:39 +0100

    Пофиксил почему не сохранялось в таблицу пользователь

--- app/signup.php
+++ app/signup.php
@@ -20,7 +20,7 @@
 
         $password = md5($password);
 
-        mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `avatar`, `balance`) VALUES (NULL, '$full_name', '$email', '$password', '$path')");
+        mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `avatar`, `balance`) VALUES (NULL, '$full_name', '$email', '$password', '$path', 0)");
         $_SESSION['message'] = 'Регистрация прошла успешно';
         header('Location: ../public/index.php');
 
